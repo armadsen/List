022883f06efe6e749017ec3b9ec10c40dc14e85f
mowerb <Zebra@Zebras-MacBook-Air.local>
2016-10-12 13:54:52 -0600

    Code improvement - reviewing each file
    - Simplified first launch instructions
    - removed print statements to first launch code
    - removed cast to NSIndexPath in several places since the type is
    already IndexPath which is correct. This change seems to have been
    necessary b/c the Swift Convert to 3.0 isn’t perfect. I forgive you.
    - Moved a function outside of tableView function and reworked the
    function to work correctly.

--- List/ItemsTableViewController.swift
+++ List/ItemsTableViewController.swift
@@ -34,13 +34,10 @@ class ItemsTableViewController: UITableViewController, SFSafariViewControllerDel
         // Launch instructions
         let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
         
-        if launchedBefore  {
-            print("Not first launch.")
-        }
-        else {
+        // Checks for first launch, and executes code if not first launch. If first launch, shows launch screen and sets UserDefaults to save status of first launch.
+        if launchedBefore == false {
             let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
             let vc = storyboard.instantiateViewController(withIdentifier: "welcome")
-            print("First launch, setting NSUserDefault.")
             UserDefaults.standard.set(true, forKey: "launchedBefore")
             present(vc, animated: true, completion: nil)
         }
@@ -57,47 +54,15 @@ class ItemsTableViewController: UITableViewController, SFSafariViewControllerDel
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemTableViewCell
         
-        let item = ItemController.sharedController.items[(indexPath as NSIndexPath).row]
+        let item = ItemController.sharedController.items[indexPath.row]
         
         cell.titleLabel.text = item.title
         cell.urlLabel.text = item.url
         
+        let (red, green, blue) = calculateColorGradient(row: CGFloat(indexPath.row))
+        cell.coloredBoxView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
         
-    // Calculate color gradient
-        
-        // Set the color values (PINK: [255, 20, 147] ORANGE: [255, 69, 0])
-        let colorTopValues: [CGFloat] = [255, 20, 147] // R G B
-        let colorBottomValues: [CGFloat] = [255, 69, 0] // R G B
-        
-        // Take each RGB value, find out which one is lower
-        let higherRed = whichNumberIsHigher(colorTopValues[0], secondValue: colorBottomValues[0])
-        let higherBlue = whichNumberIsHigher(colorTopValues[1], secondValue: colorBottomValues[1])
-        let higherGreen = whichNumberIsHigher(colorTopValues[2], secondValue: colorBottomValues[2])
-        
-        // Subtract the two and find the absolute value to get the difference.
-        let differenceRed = fabs(colorTopValues[0] - colorBottomValues[0])
-        let differenceBlue = fabs(colorTopValues[1] - colorBottomValues[1])
-        let differenceGreen = fabs(colorTopValues[2] - colorBottomValues[2])
-        
-        // Divide that difference by (the number of items in the list - 1)
-        // Add (that value * the cellRow) to the lower of the two values and divide by 255
-        // That value will be the new value that needs to be the RGB value to use
-
-        var divisor: CGFloat = 1
-        let numberOfItems = ItemController.sharedController.items.count
-        if numberOfItems > 1 {
-            divisor = CGFloat(numberOfItems - 1)
-        }
-        
-        let newRedValue = (higherRed - CGFloat((indexPath as NSIndexPath).row) * (differenceRed / divisor)) / 255
-        let newBlueValue = (higherBlue - CGFloat((indexPath as NSIndexPath).row) * (differenceBlue / divisor)) / 255
-        let newGreenValue = (higherGreen - CGFloat((indexPath as NSIndexPath).row) * (differenceGreen / divisor)) / 255
-        
-        // Set the bg color for the coloredView in each cell equal to the calculated RGB value
-        cell.coloredBoxView.backgroundColor = UIColor(red: newRedValue, green: newGreenValue, blue: newBlueValue, alpha: 1)
-    
-        
-    // Set the big letter as the first letter in Title, else first letter in URL
+        // Set the big letter as the first letter in Title, else first letter in URL
         if let bigLetter = item.title?.characters.first {
             cell.bigLetterLabel.text = String(bigLetter).capitalized
         } else if let bigLetter = item.url?.characters.first {
@@ -105,20 +70,6 @@ class ItemsTableViewController: UITableViewController, SFSafariViewControllerDel
         }
         
         return cell
-    }
-    
-    
-    // Right now it figures out which value is lowest, and then adds the numberToAdd to that value. Thus, always stacks the lower value on top/first. What it needs to do instead is find out if the higher value is on top or the lower value is on top. If the higher value is on top, it should subtract the numberToAdd from the higher value. If the lower value is on top it should add the numberToAdd to the lower value.
-    func whichNumberIsHigher(_ firstValue: CGFloat, secondValue: CGFloat) -> CGFloat {
-        var higherNumber: CGFloat = 0
-
-        if firstValue >= secondValue {
-            higherNumber = firstValue
-        } else {
-            higherNumber = secondValue
-        }
-        
-        return higherNumber
     }
     
     
@@ -165,5 +116,54 @@ class ItemsTableViewController: UITableViewController, SFSafariViewControllerDel
         
         self.present(safariVC, animated: true, completion: nil)
     }
+    
+    
+    // MARK: - Functions
+    
+    func calculateColorGradient(row: CGFloat) -> (red: CGFloat, green: CGFloat, blue: CGFloat) {
+        // Set the color values in RGB (PINK: [255, 20, 147] ORANGE: [255, 69, 0])
+        let colorTopValues: [CGFloat] = [255, 20, 147]
+        let colorBottomValues: [CGFloat] = [255, 69, 0]
+        
+        // Take each RGB value, find out which one is higher
+        let higherRed = whichNumberIsHigher(colorTopValues[0], secondValue: colorBottomValues[0])
+        let higherBlue = whichNumberIsHigher(colorTopValues[1], secondValue: colorBottomValues[1])
+        let higherGreen = whichNumberIsHigher(colorTopValues[2], secondValue: colorBottomValues[2])
+        
+        // Subtract the two and find the absolute value to get the difference.
+        let differenceRed = fabs(colorTopValues[0] - colorBottomValues[0])
+        let differenceBlue = fabs(colorTopValues[1] - colorBottomValues[1])
+        let differenceGreen = fabs(colorTopValues[2] - colorBottomValues[2])
+        
+        // Divide that difference by (the number of items in the list - 1)
+        // Add (that value * the cellRow) to the lower of the two values and divide by 255
+        // That value will be the new value that needs to be the RGB value to use
+        
+        var divisor: CGFloat = 1
+        let numberOfItems = ItemController.sharedController.items.count
+        if numberOfItems > 1 {
+            divisor = CGFloat(numberOfItems - 1)
+        }
+        
+        let newRedValue = (higherRed - row * (differenceRed / divisor)) / 255
+        let newBlueValue = (higherBlue - row * (differenceBlue / divisor)) / 255
+        let newGreenValue = (higherGreen - row * (differenceGreen / divisor)) / 255
+        
+        return (newRedValue, newGreenValue, newBlueValue)
+    }
+    
+    // Right now it figures out which value is lowest, and then adds the numberToAdd to that value. Thus, always stacks the lower value on top/first. What it needs to do instead is find out if the higher value is on top or the lower value is on top. If the higher value is on top, it should subtract the numberToAdd from the higher value. If the lower value is on top it should add the numberToAdd to the lower value.
+    func whichNumberIsHigher(_ firstValue: CGFloat, secondValue: CGFloat) -> CGFloat {
+        var higherNumber: CGFloat = 0
+        
+        if firstValue >= secondValue {
+            higherNumber = firstValue
+        } else {
+            higherNumber = secondValue
+        }
+        
+        return higherNumber
+    }
+    
     
 }
